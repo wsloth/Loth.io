4bf116dc9a482551b6c0973d3f8d3d805cc44717
Wessel Loth <wessel@loth.io>
2016-11-16 22:09:07 +0100

    improve responsiveness of page and add tagline

--- public/js/app.js
+++ public/js/app.js
@@ -5,222 +5,222 @@ var mobilecheck = function() {
   return check;
 }();
 
-// (function() {
-//     var width, height, largeHeader, canvas, ctx, points, target, animateHeader = true;
+(function() {
+    var width, height, largeHeader, canvas, ctx, points, target, animateHeader = true;
 
-//     // Amount of particles
-//     var dimensions = 20;
+    // Amount of particles
+    var dimensions = 20;
 
-//     // First circle
-//     var fRange = 20000;
-//     var fVisibility = {line: 0.3, circle: 0.6};
+    // First circle
+    var fRange = 20000;
+    var fVisibility = {line: 0.3, circle: 0.6};
 
-//     // Second circle
-//     var sRange = 30000;
-//     var sVisibility = {line: 0.2, circle: 0.3};
+    // Second circle
+    var sRange = 30000;
+    var sVisibility = {line: 0.2, circle: 0.3};
 
-//     // Third circle
-//     var tRange = 150000;
-//     var tVisibility = {line: 0.1, circle: 0.1};
+    // Third circle
+    var tRange = 150000;
+    var tVisibility = {line: 0.1, circle: 0.1};
 
-//     // Outer circle
-//     var oVisibility = {line: 0.03, circle: 0.05};
+    // Outer circle
+    var oVisibility = {line: 0.03, circle: 0.05};
 
-//     if (mobilecheck) {
-//         dimensions = 7;
-//         fRange = 7000;
-//         fVisibility = {line: 0.2, circle: 0.4};
-//         sRange = 20000;
-//         sVisibility = {line: 0.1, circle: 0.1};
-//         tRange = 60000;
-//         tVisibility = {line: 0.03, circle: 0.05};
-//     }
+    if (mobilecheck) {
+        dimensions = 7;
+        fRange = 7000;
+        fVisibility = {line: 0.2, circle: 0.4};
+        sRange = 20000;
+        sVisibility = {line: 0.1, circle: 0.1};
+        tRange = 60000;
+        tVisibility = {line: 0.03, circle: 0.05};
+    }
 
 
-//     // Main
-//     initHeader();
-//     initAnimation();
-//     addListeners();
+    // Main
+    initHeader();
+    initAnimation();
+    addListeners();
 
-//     function initHeader() {
-//         width = window.innerWidth;
-//         height = window.innerHeight;
-//         target = {x: width/2, y: height/2};
+    function initHeader() {
+        width = window.innerWidth;
+        height = window.innerHeight;
+        target = {x: width/2, y: height/2};
 
-//         // largeHeader = document.getElementById('large-header');
-//         // largeHeader.style.height = height+'px';
+        // largeHeader = document.getElementById('large-header');
+        // largeHeader.style.height = height+'px';
 
-//         canvas = document.getElementById('demo-canvas');
-//         canvas.width = width;
-//         canvas.height = height;
-//         ctx = canvas.getContext('2d');
+        canvas = document.getElementById('demo-canvas');
+        canvas.width = width;
+        canvas.height = height;
+        ctx = canvas.getContext('2d');
 
-//         // Load less on mobile
-//         var dimensions = 20;
-//         if (mobilecheck) {
-//             dimensions = 7;
-//         }
+        // Load less on mobile
+        var dimensions = 20;
+        if (mobilecheck) {
+            dimensions = 7;
+        }
 
-//         // create points
-//         points = [];
-//         for(var x = 0; x < width; x = x + width/dimensions) {
-//             for(var y = 0; y < height; y = y + height/dimensions) {
-//                 var px = x + Math.random()*width/dimensions;
-//                 var py = y + Math.random()*height/dimensions;
-//                 var p = {x: px, originX: px, y: py, originY: py };
-//                 points.push(p);
-//             }
-//         }
+        // create points
+        points = [];
+        for(var x = 0; x < width; x = x + width/dimensions) {
+            for(var y = 0; y < height; y = y + height/dimensions) {
+                var px = x + Math.random()*width/dimensions;
+                var py = y + Math.random()*height/dimensions;
+                var p = {x: px, originX: px, y: py, originY: py };
+                points.push(p);
+            }
+        }
 
-//         // for each point find the 5 closest points
-//         for(var i = 0; i < points.length; i++) {
-//             var closest = [];
-//             var p1 = points[i];
-//             for(var j = 0; j < points.length; j++) {
-//                 var p2 = points[j]
-//                 if(!(p1 == p2)) {
-//                     var placed = false;
-//                     for(var k = 0; k < 5; k++) {
-//                         if(!placed) {
-//                             if(closest[k] == undefined) {
-//                                 closest[k] = p2;
-//                                 placed = true;
-//                             }
-//                         }
-//                     }
+        // for each point find the 5 closest points
+        for(var i = 0; i < points.length; i++) {
+            var closest = [];
+            var p1 = points[i];
+            for(var j = 0; j < points.length; j++) {
+                var p2 = points[j]
+                if(!(p1 == p2)) {
+                    var placed = false;
+                    for(var k = 0; k < 5; k++) {
+                        if(!placed) {
+                            if(closest[k] == undefined) {
+                                closest[k] = p2;
+                                placed = true;
+                            }
+                        }
+                    }
 
-//                     for(var k = 0; k < 5; k++) {
-//                         if(!placed) {
-//                             if(getDistance(p1, p2) < getDistance(p1, closest[k])) {
-//                                 closest[k] = p2;
-//                                 placed = true;
-//                             }
-//                         }
-//                     }
-//                 }
-//             }
-//             p1.closest = closest;
-//         }
+                    for(var k = 0; k < 5; k++) {
+                        if(!placed) {
+                            if(getDistance(p1, p2) < getDistance(p1, closest[k])) {
+                                closest[k] = p2;
+                                placed = true;
+                            }
+                        }
+                    }
+                }
+            }
+            p1.closest = closest;
+        }
 
-//         // assign a circle to each point
-//         for(var i in points) {
-//             var c = new Circle(points[i], 2+Math.random()*2, 'rgba(255,255,255,0.3)');
-//             points[i].circle = c;
-//         }
-//     }
+        // assign a circle to each point
+        for(var i in points) {
+            var c = new Circle(points[i], 2+Math.random()*2, 'rgba(255,255,255,0.3)');
+            points[i].circle = c;
+        }
+    }
 
-//     // Event handling
-//     function addListeners() {
-//         if(!('ontouchstart' in window)) {
-//             window.addEventListener('mousemove', mouseMove);
-//         }
-//         window.addEventListener('scroll', scrollCheck);
-//         window.addEventListener('resize', resize);
-//     }
+    // Event handling
+    function addListeners() {
+        if(!('ontouchstart' in window)) {
+            window.addEventListener('mousemove', mouseMove);
+        }
+        window.addEventListener('scroll', scrollCheck);
+        window.addEventListener('resize', resize);
+    }
 
-//     function mouseMove(e) {
-//         var posx = posy = 0;
-//         if (e.pageX || e.pageY) {
-//             posx = e.pageX;
-//             posy = e.pageY;
-//         }
-//         else if (e.clientX || e.clientY)    {
-//             posx = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
-//             posy = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
-//         }
-//         target.x = posx;
-//         target.y = posy;
-//     }
+    function mouseMove(e) {
+        var posx = posy = 0;
+        if (e.pageX || e.pageY) {
+            posx = e.pageX;
+            posy = e.pageY;
+        }
+        else if (e.clientX || e.clientY)    {
+            posx = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+            posy = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+        }
+        target.x = posx;
+        target.y = posy;
+    }
 
-//     function scrollCheck() {
-//         if(document.body.scrollTop > height) animateHeader = false;
-//         else animateHeader = true;
-//     }
+    function scrollCheck() {
+        if(document.body.scrollTop > height) animateHeader = false;
+        else animateHeader = true;
+    }
 
-//     function resize() {
-//         width = window.innerWidth;
-//         height = window.innerHeight;
-//         // largeHeader.style.height = height+'px';
-//         canvas.width = width;
-//         canvas.height = height;
-//     }
+    function resize() {
+        width = window.innerWidth;
+        height = window.innerHeight;
+        // largeHeader.style.height = height+'px';
+        canvas.width = width;
+        canvas.height = height;
+    }
 
-//     // animation
-//     function initAnimation() {
-//         animate();
-//         for(var i in points) {
-//             shiftPoint(points[i]);
-//         }
-//     }
+    // animation
+    function initAnimation() {
+        animate();
+        for(var i in points) {
+            shiftPoint(points[i]);
+        }
+    }
 
-//     function animate() {
-//         if(animateHeader) {
-//             ctx.clearRect(0,0,width,height);
-//             for(var i in points) {
-//                 // detect points in range
-//                 if(Math.abs(getDistance(target, points[i])) < fRange) {
-//                     points[i].active = fVisibility.line;
-//                     points[i].circle.active = fVisibility.circle;
-//                 } else if(Math.abs(getDistance(target, points[i])) < sRange) {
-//                     points[i].active = sVisibility.line;
-//                     points[i].circle.active = sVisibility.circle;
-//                 } else if(Math.abs(getDistance(target, points[i])) < tRange) {
-//                     points[i].active = tVisibility.line;
-//                     points[i].circle.active = tVisibility.circle;
-//                 } else {
-//                     points[i].active = oVisibility.line;
-//                     points[i].circle.active = oVisibility.circle;
-//                 }
+    function animate() {
+        if(animateHeader) {
+            ctx.clearRect(0,0,width,height);
+            for(var i in points) {
+                // detect points in range
+                if(Math.abs(getDistance(target, points[i])) < fRange) {
+                    points[i].active = fVisibility.line;
+                    points[i].circle.active = fVisibility.circle;
+                } else if(Math.abs(getDistance(target, points[i])) < sRange) {
+                    points[i].active = sVisibility.line;
+                    points[i].circle.active = sVisibility.circle;
+                } else if(Math.abs(getDistance(target, points[i])) < tRange) {
+                    points[i].active = tVisibility.line;
+                    points[i].circle.active = tVisibility.circle;
+                } else {
+                    points[i].active = oVisibility.line;
+                    points[i].circle.active = oVisibility.circle;
+                }
 
-//                 drawLines(points[i]);
-//                 points[i].circle.draw();
-//             }
-//         }
-//         requestAnimationFrame(animate);
-//     }
+                drawLines(points[i]);
+                points[i].circle.draw();
+            }
+        }
+        requestAnimationFrame(animate);
+    }
 
-//     function shiftPoint(p) {
-//         TweenLite.to(p, 1+1*Math.random(), {x:p.originX-50+Math.random()*100,
-//             y: p.originY-50+Math.random()*100, ease:Circ.easeInOut,
-//             onComplete: function() {
-//                 shiftPoint(p);
-//             }});
-//     }
+    function shiftPoint(p) {
+        TweenLite.to(p, 1+1*Math.random(), {x:p.originX-50+Math.random()*100,
+            y: p.originY-50+Math.random()*100, ease:Circ.easeInOut,
+            onComplete: function() {
+                shiftPoint(p);
+            }});
+    }
 
-//     // Canvas manipulation
-//     function drawLines(p) {
-//         if(!p.active) return;
-//         for(var i in p.closest) {
-//             ctx.beginPath();
-//             ctx.moveTo(p.x, p.y);
-//             ctx.lineTo(p.closest[i].x, p.closest[i].y);
-//             ctx.strokeStyle = 'rgba(156,217,249,'+ p.active+')';
-//             ctx.stroke();
-//         }
-//     }
+    // Canvas manipulation
+    function drawLines(p) {
+        if(!p.active) return;
+        for(var i in p.closest) {
+            ctx.beginPath();
+            ctx.moveTo(p.x, p.y);
+            ctx.lineTo(p.closest[i].x, p.closest[i].y);
+            ctx.strokeStyle = 'rgba(156,217,249,'+ p.active+')';
+            ctx.stroke();
+        }
+    }
 
-//     function Circle(pos,rad,color) {
-//         var _this = this;
+    function Circle(pos,rad,color) {
+        var _this = this;
 
-//         // constructor
-//         (function() {
-//             _this.pos = pos || null;
-//             _this.radius = rad || null;
-//             _this.color = color || null;
-//         })();
+        // constructor
+        (function() {
+            _this.pos = pos || null;
+            _this.radius = rad || null;
+            _this.color = color || null;
+        })();
 
-//         this.draw = function() {
-//             if(!_this.active) return;
-//             ctx.beginPath();
-//             ctx.arc(_this.pos.x, _this.pos.y, _this.radius, 0, 2 * Math.PI, false);
-//             ctx.fillStyle = 'rgba(156,217,249,'+ _this.active+')';
-//             ctx.fill();
-//         };
-//     }
+        this.draw = function() {
+            if(!_this.active) return;
+            ctx.beginPath();
+            ctx.arc(_this.pos.x, _this.pos.y, _this.radius, 0, 2 * Math.PI, false);
+            ctx.fillStyle = 'rgba(156,217,249,'+ _this.active+')';
+            ctx.fill();
+        };
+    }
 
-//     // Util
-//     function getDistance(p1, p2) {
-//         return Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2);
-//     }
+    // Util
+    function getDistance(p1, p2) {
+        return Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2);
+    }
     
-// })();
+})();
